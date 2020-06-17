6768aaacc4ccf9a4a43b8d63f9feeb2d21411b16
Vivek-Kamble <vvek.kamble@gmail.com>
2020-06-17 21:29:04 +0530

    Updates

---
+++ precache-manifest.6318ae1ca460747940497bde07cd86d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78ad45e91d78675f3b7874b5b8b3035f",
+    "url": "/vivek-react-mobile-store/index.html"
+  },
+  {
+    "revision": "a374fc5af94c58a1a86a",
+    "url": "/vivek-react-mobile-store/static/css/main.ad528ef3.chunk.css"
+  },
+  {
+    "revision": "d222bf11ce8d31a1ad4f",
+    "url": "/vivek-react-mobile-store/static/js/2.bcd433fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vivek-react-mobile-store/static/js/2.bcd433fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a374fc5af94c58a1a86a",
+    "url": "/vivek-react-mobile-store/static/js/main.931ff38c.chunk.js"
+  },
+  {
+    "revision": "032c3c8f3bf4b4c0216a",
+    "url": "/vivek-react-mobile-store/static/js/runtime-main.214a3a2f.js"
+  },
+  {
+    "revision": "11da2c66f8b9ae430638749bc648e673",
+    "url": "/vivek-react-mobile-store/static/media/login.11da2c66.png"
+  }
+]);
